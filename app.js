36f24da76614d218fe736cd2d4169bf91adb8713
hamsterpants <kevinhoejgaard@gmail.com>
2024-11-18 14:10:29 +0000

    setting env vars instead

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
  */
 module.exports = (app) => {
     app.log("Yay! The app was loaded!");
-  
     app.on("issues.opened", async (context) => {
       return context.octokit.issues.createComment(
         context.issue({ body: "Hello, World!" })
