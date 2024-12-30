a9ebd41e9b512f176f2305508c7c61f6ede061fa
Hamsterpants <kevinhoejgaard@gmail.com>
2024-12-30 12:21:00 +0000

    set webhookPath for gcf

--- function.js
+++ function.js
@@ -1,4 +1,8 @@
 const { createNodeMiddleware, createProbot } = require("probot");
 const app = require("./app");
 
-exports.probotApp = createNodeMiddleware(app, { probot: createProbot() });
+// Create the Probot middleware
+exports.probotApp = createNodeMiddleware(app, {
+  probot: createProbot(),
+  webhooksPath: "/my-app", // Ensure this matches your base path
+});
