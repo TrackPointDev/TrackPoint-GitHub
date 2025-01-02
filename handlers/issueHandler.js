3f73252b7e4188776c08fbcbd4c87a3153e69b4a
Hamsterpants <kevinhoejgaard@gmail.com>
2025-01-02 21:38:16 +0000

    add issueHandler post/put logic

--- handlers/issueHandler.js
+++ handlers/issueHandler.js
@@ -1,14 +1,23 @@
 const { validateJsonObject } = require("../utils/jsonValidator");
+const axios = require('axios');
+const baseUrl = process.env.BACKEND_URL;
+const customRoute = '/epic/task';
+
+//const url = baseUrl + customRoute;
+const url = "http://localhost:3000/test";
 
 module.exports = (app) => {
     app.on("issues.opened", async (context) => {
+    
+
         const { owner, repo } = context.repo();
         const issueNumber = context.payload.issue.number;
         const schemaUrl = `https://raw.githubusercontent.com/TrackPointDev/TrackPoint-json-schemas/refs/heads/main/json-schemas/task_schema.json`
 
         //TODO make priority and storypoint take from project
         const jsonObject = {
-            "comments" : "Comments on the task not on GitHub",
+            "repoOwner": owner,
+            "repo": repo,
             "description" : context.payload.issue.body || "",
             "issueID" : issueNumber.toString(),
             "priority" : "High",
@@ -17,27 +26,24 @@ module.exports = (app) => {
         };
 
         const isValid = await validateJsonObject(jsonObject, schemaUrl);
+
         if (!isValid) {
             app.log.error(`Failed to validate JSON for issue #${issueNumber}`);
             return;
         }
         
-        //TODO send to backend
+        
         try {
-            const createComment = await context.octokit.issues.createComment({
-                owner,
-                repo,
-                issue_number: issueNumber,
-                body: JSON.stringify(jsonObject, null, 2)
-            });
-            
-            app.log.info(`Issue #${issueNumber} updated`, createComment.data );
+            // Send the JSON object to the backend
+            const response = await axios.post(url, jsonObject);
+            app.log.info(`Successfully post data for issue #${issueNumber} to backend`, response.data);
         } catch (error) {
-            app.log.error(`Failed to update issue #${issueNumber}`, error);
+            app.log.error(`Failed to post data for issue #${issueNumber} to backend`, error);
         }
     });
 
     app.on("issues.edited", async (context) => {
+
         // Check if the event was triggered by a bot
         if (context.isBot) {
             app.log.info('Ignoring event triggered by a bot');
@@ -50,34 +56,31 @@ module.exports = (app) => {
        
         //TODO make priority and storypoint take from project
         const jsonObject = {
-            "comments" : "Comments on the task not on GitHub",
+            "repoOwner": owner,
+            "repo": repo,
             "description" : context.payload.issue.body || "",
             "issueID" : issueNumber.toString(),
             "priority" : "High",
             "story_point" : 8,
             "title" : context.payload.issue.title
         };
-
+          
         const isValid = await validateJsonObject(jsonObject, schemaUrl);
         if (!isValid) {
             app.log.error(`Failed to validate JSON for issue #${issueNumber}`);
             return;
         }
 
-        //TODO send to backend
         try {
-            const createComment = await context.octokit.issues.createComment({
-                owner,
-                repo,
-                issue_number: issueNumber,
-                body: JSON.stringify(jsonObject, null, 2)
-            });
-            
-            app.log.info(`Issue #${issueNumber} updated`, createComment.data );
+            // Send the JSON object to the backend
+            const response = await axios.put(url, jsonObject);
+            app.log.info(`Successfully put data for issue #${issueNumber} to backend`, response.data);
         } catch (error) {
-            app.log.error(`Failed to update issue #${issueNumber}`, error);
+            app.log.error(`Failed to put data for issue #${issueNumber} to backend`, error);
         }
     });
+    
+    app.on("issues.deleted", async (context) => {
 
-
+    })
 };
