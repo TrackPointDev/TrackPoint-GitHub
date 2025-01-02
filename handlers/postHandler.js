8065d97a225bb9cb05c996c43e79d58604262c02
Hamsterpants <kevinhoejgaard@gmail.com>
2025-01-02 21:39:12 +0000

    expand posthander logic

--- handlers/postHandler.js
+++ handlers/postHandler.js
@@ -1,7 +1,48 @@
 const { validateJsonObject } = require("../utils/jsonValidator");
-const { Probot } = require('probot');
 
-exports.submitData = async (req, res) => {
+exports.test = async (req, res, app) => {
+    const payload = req.body;
+    app.log(payload)
+    res.status(200).send("request received successfully")
+}
+
+exports.epicUpdate = async (req, res, app) => {
+    const payload = req.body;
+    const {repoOwner, repoName, secret, installationID, tasks } = payload;
+
+    if (secret !== process.env.WEBHOOK_SECRET) {
+        return res.status(403).send('Forbidden');
+    }
+
+    try {
+        // Use the Probot app instance to authenticate
+        const github = await app.auth(installationID);
+
+        for (const task of tasks) {
+            const issueTitle = `${task.title}`
+            const issueBody = `${task.description}`
+            const priority = `${task.priority}`
+            const story_point = `${task.story_point}`
+            // Update an existing issue using the authenticated installation
+            const response = await github.issues.update({
+                owner: repoOwner,
+                repo: repoName,
+                issue_number: task.issueID, // Use the existing issue ID
+                title: issueTitle,
+                body: issueBody,
+                labels: ['priority:' + priority, 'story point:' + story_point]
+            });
+        }
+        
+        res.status(200).send(`${tasks.length} issues updated successfully.`);
+
+    } catch (error) {
+        console.error('Error updating issues:', error);
+        res.status(500).send('Error updating issues');
+    }
+};
+
+exports.taskUpdate = async (req, res, app) => {
     // Handle the POST request data
     const payload = req.body;
     const schemaUrl = `https://raw.githubusercontent.com/TrackPointDev/TrackPoint-json-schemas/refs/heads/main/json-schemas/task_schema.json`
@@ -12,16 +53,37 @@ exports.submitData = async (req, res) => {
         const isValid = await validateJsonObject(payload, schemaUrl);
         if (!isValid) {
             console.error(`Failed to validate JSON for issue #${issueNumber}`);
+            app.log(`Failed to validate JSON for issue #${issueNumber}`);
             return res.status(400).send('Failed to validate JSON');
         }
-        return res.send('Data submitted successfully');
+        app.log("json is valid")
+        const {repoOwner, repoName, installationID, description, issueID, priority, story_point, title} = payload
+        const github = await app.auth(installationID)
+
+        const response = await github.issues.update({
+            owner: repoOwner,
+            repo: repoName,
+            issue_number: issueID,
+            title: title,
+            body: description,
+            labels: ['priority:' + priority, 'story point:' + story_point]
+        });
+
+        if (response.status === 200) {
+            console.log(`Issue #${issueID} updated successfully.`);
+            return res.send('Issue updated successfully');
+        } else {
+            console.error(`Failed to update issue #${issueID}:`, response);
+            return res.status(response.status).send('Failed to update issue');
+        }
+
     } catch (error) {
         console.error('Error processing request:', error);
         return (error); // Pass the error to Express error handler
     }
 };
 
-exports.initialSetup = async (req, res, app) => {
+exports.epicSetup = async (req, res, app) => {
     const payload = req.body;
     const {repoOwner, repoName, secret, installationID, tasks } = payload;
 
@@ -38,12 +100,15 @@ exports.initialSetup = async (req, res, app) => {
         for (const task of tasks) {
             const issueTitle = `${task.title}`
             const issueBody = `${task.description}`
+            const priority = `${task.priority}`
+            const story_point = `${task.story_point}`
             // Create an issue using the authenticated installation
             const response = await github.issues.create({
                 owner: repoOwner,
                 repo: repoName,
                 title: issueTitle,
                 body: issueBody,
+                labels: ['priority:' + priority, 'story point:' + story_point]
             });
 
             issueList.push({ title: issueTitle, number: response.data.number });
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,14 +1,66 @@
 const postHandler = require('../handlers/postHandler');
 const express = require('express');
 
-module.exports = (router) => {
-    router.use(express.json());
-
-    router.post('/submit', postHandler.submitData);
-};
-
 module.exports = (router, probot) => {
     router.use(express.json());
 
-    router.post('/initial-setup', (req, res) => postHandler.initialSetup(req, res, probot));
+    // Middleware for request validation (optional)
+    const validateRequest = (req, res, next) => {
+        // Add validation logic here
+        next();
+    };
+    
+    //Dummy routes for internal calls test
+    router.post('/test', validateRequest, (req, res) => {
+        try {
+            postHandler.test(req, res, probot);
+        } catch (error) {
+            console.error('Error handling /test', error);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+    router.put('/test', validateRequest, (req, res) => {
+        try {
+            postHandler.test(req, res, probot);
+        } catch (error) {
+            console.error('Error handling /test', error);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+    router.delete('/test', validateRequest, (req, res) => {
+        try {
+            postHandler.test(req, res, probot);
+        } catch (error) {
+            console.error('Error handling /test', error);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+    
+
+    router.post('/epic/update', validateRequest, (req, res) => {
+        try {
+            postHandler.epicUpdate(req, res, probot);
+        } catch (error) {
+            console.error('Error handling /epic/update', error);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+
+    router.post('/task/update', validateRequest, (req, res) => {
+        try {
+            postHandler.submitData(req, res, probot);
+        } catch (error) {
+            console.error('Error handling /task/update:', error);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+
+    router.post('/epic/setup', validateRequest, (req, res) => {
+        try {
+            postHandler.epicSetup(req, res, probot);
+        } catch (error) {
+            console.error('Error handling /epic/setup:', error);
+            res.status(500).send('Internal Server Error');
+        }
+    });
 };
